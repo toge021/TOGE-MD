385e80be6920409633342091ae8a6637914c1524
𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸 <afrinode.tech@gmail.com>
2026-04-28 06:27:19 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+// config.js
+export default {
+  PREFIX: ".",
+  OWNER_NUMBER: "24176209643",
+  OWNER_NAME: "𝚃𝙾𝙶𝙴 𝙸𝙽𝚄𝙼𝙰𝙺𝙸",
+  BOT_NAME: "𝚃𝙾𝙶𝙴-𝙼𝙳-𝚅𝟻",
+  SESSION_ID: "",
+  AUTO_READ: false,
+  AUTO_LIKE_STATUS: false,
+  AUTO_TYPING: false,
+  AUTO_RECORDING: false,
+  AUTO_TYPING_BEFORE_REPLY: false,
+  LOG_LEVEL: "info",
+  RECONNECT_DELAY: 5000,
+  DEBUG_MODE: true,
+  DOSSIER_AUTH: "sessions",
+  GITHUB_USERNAME: "toge021",
+  GITHUB_TOKEN: "",
+  AUTO_PAIR_ON_START: true
+};
